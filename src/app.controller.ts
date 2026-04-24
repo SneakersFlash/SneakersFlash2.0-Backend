89f90bea8edd7026cd0ae6a94d8615bc6c2cac68
paisalll <faizaltriasaa@gmail.com>
2026-04-24 23:56:44 +0700

    feat: prod testing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { SkipThrottle } from '@nestjs/throttler';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  
+  @SkipThrottle() // Memastikan endpoint ini kebal dari rate limit
+  @Get('health')
+  checkHealth() {
+    return {
+      status: 'ok',
+      message: 'SneakersFlash API is running gracefully 🚀',
+      timestamp: new Date().toISOString(),
+    };
   }
 }
